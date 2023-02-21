f56687d3c7d21cdc8057cd69325ab59c7818cb04
aachal01 <95211796+aachal01@users.noreply.github.com>
2023-02-21 08:59:34 +0000

    post routes setup

---
+++ server/routes/posts.js
@@ -0,0 +1,14 @@
+import express from "express";
+import { getFeedposts, getUserPosts, likePost } from "../controllers/posts.js";
+import { verifyToken } from "../middleware/auth.js";
+
+const router = express.Router();
+
+/* READ */
+router.get("/", verifyToken, getFeedposts);
+router.get("/:userId/posts", verifyToken, getUserPosts);
+
+/* UPDATE */
+router.patch("/:id/like", verifyToken, likePost);
+
+export default router;
