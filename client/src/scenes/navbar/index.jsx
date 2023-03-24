2c9975db8bcce1881306e7668c9ccc2218ab2995
aachal01 <95211796+aachal01@users.noreply.github.com>
2023-03-24 14:11:25 +0000

    reg working...

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -56,7 +56,7 @@ const Navbar = () => {
             },
           }}
         >
-          WeShare
+          Sociopedia
         </Typography>
         {isNonMobileScreens && (
           <FlexBetween
